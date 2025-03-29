0b39adf801e14202860514665e335fcca244667d
Dina Samatova <MY_NAME@example.com>
2025-03-29 17:29:15 +0100

    Simple restaurant navigation 2

--- IISSI2-IS-Frontend-Owner/src/screens/restaurants/RestaurantsScreen.js
+++ IISSI2-IS-Frontend-Owner/src/screens/restaurants/RestaurantsScreen.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { Button, Text, View } from 'react-native'
 
-export default function RestaurantsScreen() {
+export default function RestaurantsScreen({ navigation }) {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Restaurants</Text>
+            <Text>Random Restaurant</Text>
+            <Button
+                onPress={() => {
+                    navigation.navigate('RestaurantDetailScreen', { id: Math.floor(Math.random() * 100) })
+                }}
+                title="Go to Random Restaurant Details"
+            />
         </View>
     )
 }
